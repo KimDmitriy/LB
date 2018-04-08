9a2916ca87c46a875ccc6c5001941430c1251451
KimDmitriy <37148922+KimDmitriy@users.noreply.github.com>
2018-04-08 20:55:03 +0700

    Update untitled03.m

--- untitled03.m
+++ untitled03.m
@@ -1,21 +1,8 @@
-window = 3;
-alpha=2/(window+1);
+% R = real(s);
+Rm = min(R);
+delta = 0.05;
+tp = (1/abs(Rm))*(log(1/delta))
 
-Nx = length(X);
-EMAx = zeros(1, Nx);
-EMAx(1) = X(1);
-for i=2:Nx
-     EMAx(i) = alpha*X(i) + (1-alpha)*EMAx(i-1);
-end
-
-YwEMA = EMAx*C + U*D + N*F;
-Ny = length(YwEMA);
-EMAy = zeros(1, Ny);
-EMAy(1) = YwEMA(1);
-for i=2:Ny
-     EMAy(i) = alpha*YwEMA(i) + (1-alpha)*EMAy(i-1);
-end
-
-plot(Y);
-hold on;
-plot(EMAy);
+Ym = max(Y);
+Yust = Y(length(Y), 1);
+Sigma = ((Ym-Yust)/Yust)*100
