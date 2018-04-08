0dbb2a29bdc36c10d1189ca89d57cb93c9cf0706
KimDmitriy <37148922+KimDmitriy@users.noreply.github.com>
2018-04-08 20:54:39 +0700

    Update untitled02.m

--- untitled02.m
+++ untitled02.m
@@ -1,17 +1,11 @@
-I = eye(6,6);
+l = length(A);
+I = eye(l,l);
 k = sym('k');
 d = det(k*I-A);
 s = solve(d,k);
 s = double(s);
 s1 = abs(s);
 
-I = eye(6,6);
-k = sym('k');
-d = det(k*I-A);
-s = solve(d,k);
-s = double(s);
-s1 = abs(s);
-%-----------------------------------------
 flag1=0;
 flag2=0;
 
@@ -25,7 +19,7 @@ else
 flag1=flag1+1;
 end;
 end
-%
+%-----------------------------------------
 if Im == 0
     flag2=1;
 end;
@@ -42,12 +36,3 @@ if flag1>0 && flag2>0
 else
     disp('Система устойчива')
 end;
-%-----------------------------------------
-R = real(s);
-Rm = min(R);
-delta = 0.05;
-tp = (1/abs(Rm))*(log(1/delta))
-
-Ym = max(Y);
-Yust = Y(1,length(Y));
-Sigma = ((Ym-Yust)/Yust)*100
